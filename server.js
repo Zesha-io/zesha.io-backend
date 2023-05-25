53ed115cb64e54d117d1ebb4925020bec75aaa47
Paul O <pauloladimeji@gmail.com>
2023-05-25 20:22:05 +0400

    build allow

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const corsOptions = {
     // origin: 'http://localhost:8081',
-    origin: process.env.CORS_ALLOW,
+    origin: process.env.FRONT_URL,
     optionsSuccessStatus: 200,
 };
 
